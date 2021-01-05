500fc5a0602d602c749242d02167a5aff754baac
Edinson Bravo <edinson@precompro.com>
2021-01-04 21:41:36 -0500

    se crea controlador para el crud de eventos

---
+++ src/Controller/EventController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Event;
+
+class EventController extends AbstractController
+{
+    /**
+     * @Route("/event", name="event")
+     */
+    public function index(): Response
+    {
+        return $this->render('event/index.html.twig', [
+            'controller_name' => 'EventController',
+        ]);
+    }
+}
