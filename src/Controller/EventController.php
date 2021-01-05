a2ed7caec6238ed31bc79f26dd06b43e8123b29e
Edinson Bravo <edinson@precompro.com>
2021-01-05 12:14:51 -0500

    listo el controller de eventos

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,22 +2,27 @@
 
 namespace App\Controller;
 
-use App\Entity\Event;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Services\EventService;
 
 class EventController extends AbstractController
 {
     /**
-     * @Route("/", name="event")
+     * @Route("/", name="index")
      */
-    public function index(): Response
+    public function index(EventService $eventService): Response
     {
-        $event_lists = $this->getDoctrine()->getRepository(Event::class);
-        $events      = $event_lists->findAll();
+    	try {
+    		$initial = date('Y-m-d', strtotime('-30 days')) . ' 00:00:00';
+    		$finish = date('Y-m-d') . ' 23:59:59';
+        	$events = $eventService->eventList($initial, $finish);
+    	} catch (Exception $e) {
+			$events = [];
+    	}
         return $this->render('event/index.html.twig', [
-            'events'          => $events
+            'events' => $events
         ]);
     }
 }
